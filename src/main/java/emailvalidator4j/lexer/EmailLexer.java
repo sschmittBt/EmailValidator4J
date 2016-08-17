78bbf0bd8fa10b4d9d3aa9931e3c9f761d59322c
Stefan Schmitt <stefan.schmitt@braintags.de>
2016-08-17 15:45:45 +0200

    Reduced JDK requirement to 1.7

--- src/main/java/emailvalidator4j/lexer/EmailLexer.java
+++ src/main/java/emailvalidator4j/lexer/EmailLexer.java
@@ -1,150 +1,152 @@
 package emailvalidator4j.lexer;
 
-import emailvalidator4j.lexer.exception.TokenNotFound;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import emailvalidator4j.lexer.exception.TokenNotFound;
+
 public class EmailLexer {
-    private boolean invalidTokens = false;
-    private String lexedText = "";
-    private int position = 0;
-    private Optional<TokenInterface> current = Optional.empty();
-    private final List<TokenInterface> tokens = new ArrayList<>();
+  private boolean                    invalidTokens = false;
+  private String                     lexedText     = "";
+  private int                        position      = 0;
+  private TokenInterface             current       = null;
+  private final List<TokenInterface> tokens        = new ArrayList<>();
 
-    public void lex(String input) {
-        Pattern pattern = Pattern.compile(
-                "([a-zA-Z_]+[46]?)|([0-9]+)|(\r\n)|(::)|(\\s+?)|(.)|(\\p{Cc}+)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
-        );
-        Matcher matcher = pattern.matcher(input);
+  public void lex(String input) {
+    Pattern pattern = Pattern.compile("([a-zA-Z_]+[46]?)|([0-9]+)|(\r\n)|(::)|(\\s+?)|(.)|(\\p{Cc}+)",
+        Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    Matcher matcher = pattern.matcher(input);
 
-        this.reset();
-        while(matcher.find()) {
-            String text = input.substring(matcher.start(), matcher.end());
-            TokenInterface token = Tokens.get(text);
+    this.reset();
+    while (matcher.find()) {
+      String text = input.substring(matcher.start(), matcher.end());
+      TokenInterface token = Tokens.get(text);
 
-            if (this.isInvalidToken(token)) {
-                this.invalidTokens = true;
-            }
+      if (this.isInvalidToken(token)) {
+        this.invalidTokens = true;
+      }
 
-            this.tokens.add(Tokens.get(text));
-        }
-
-        if (!this.tokens.isEmpty()) {
-            this.current = Optional.of(this.tokens.get(this.position));
-            this.lexedText += this.tokens.get(this.position).getText();
-        }
+      this.tokens.add(Tokens.get(text));
     }
 
-    /**
-     * Warning! This method IS NOT side effect free, it changes lexer state.
-     */
-    private void reset() {
-        this.lexedText = "";
-        this.position = 0;
-        this.current = Optional.empty();
-        this.tokens.clear();
-        this.invalidTokens = false;
+    if (!this.tokens.isEmpty()) {
+      this.current = this.tokens.get(this.position);
+      this.lexedText += this.tokens.get(this.position).getText();
+    }
+  }
+
+  /**
+   * Warning! This method IS NOT side effect free, it changes lexer state.
+   */
+  private void reset() {
+    this.lexedText = "";
+    this.position = 0;
+    this.current = null;
+    this.tokens.clear();
+    this.invalidTokens = false;
+  }
+
+  private boolean isInvalidToken(TokenInterface token) {
+    return token.getName().equals(Tokens.INVALID);
+  }
+
+  public TokenInterface getCurrent() {
+    if (current != null)
+      return current;
+    else
+      return new Token("", "");
+  }
+
+  /**
+   * Warning! This method IS NOT side effect free, it changes lexer state.
+   */
+  public void next() {
+    this.position++;
+    if (!this.isAtEnd()) {
+      this.current = this.tokens.get(this.position);
+      this.lexedText += this.tokens.get(this.position).getText();
+    }
+  }
+
+  public boolean isAtEnd() {
+    return this.position >= this.tokens.size();
+  }
+
+  public boolean isNextToken(TokenInterface token) {
+    if (this.tokens.size() <= this.position + 1) {
+      return false;
+    }
+    return this.tokens.get(this.position + 1).equals(token);
+  }
+
+  public boolean isNextToken(List< ? extends TokenInterface> tokens) {
+    for (TokenInterface token : tokens) {
+      if (isNextToken(token)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean find(TokenInterface token) {
+    if (this.tokens.size() <= this.position + 1) {
+      return false;
     }
 
-    private boolean isInvalidToken(TokenInterface token) {
-        return token.getName().equals(Tokens.INVALID);
+    int lookahead = this.position + 1;
+
+    while (lookahead < this.tokens.size()) {
+      if (token.equals(this.tokens.get(lookahead))) {
+        return true;
+      }
+      lookahead++;
+    }
+    return false;
+  }
+
+  /**
+   * Warning! This method IS NOT side effect free, it changes lexer state.
+   */
+  public void moveTo(TokenInterface token) throws TokenNotFound {
+    if (this.tokens.size() <= this.position + 1) {
+      return;
     }
 
-    public TokenInterface getCurrent() {
-        return this.current.orElse(new Token("", ""));
+    while (this.position < this.tokens.size()) {
+      if (token.equals(this.tokens.get(this.position))) {
+        return;
+      }
+      this.next();
     }
 
-    /**
-     * Warning! This method IS NOT side effect free, it changes lexer state.
-     */
-    public void next() {
-        this.position ++;
-        if (!this.isAtEnd()) {
-            this.current = Optional.of(this.tokens.get(this.position));
-            this.lexedText += this.tokens.get(this.position).getText();
-        }
+    throw new TokenNotFound();
+  }
+
+  public TokenInterface getPrevious() {
+    int previousPosition = this.position - 1 >= 0 ? this.position - 1 : 0;
+    return this.tokens.get(previousPosition);
+  }
+
+  public boolean hasInvalidTokens() {
+    return this.invalidTokens;
+  }
+
+  public String lexedText() {
+    return this.lexedText;
+  }
+
+  @Override
+  public String toString() {
+    int tempPosition = this.position;
+    String tokensTexts = "";
+
+    while (tempPosition < this.tokens.size()) {
+      tokensTexts += this.tokens.get(tempPosition).getText();
+      tempPosition++;
     }
 
-    public boolean isAtEnd() {
-        return this.position >= this.tokens.size();
-    }
-
-    public boolean isNextToken(TokenInterface token) {
-        if (this.tokens.size() <= this.position + 1) {
-            return false;
-        }
-        return this.tokens.get(this.position + 1).equals(token);
-    }
-
-    public boolean isNextToken(List<TokenInterface> tokens) {
-        for (TokenInterface token : tokens) {
-            if (isNextToken(token)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean find(TokenInterface token) {
-        if (this.tokens.size() <= this.position + 1) {
-            return false;
-        }
-
-        int lookahead = this.position + 1;
-
-        while (lookahead < this.tokens.size()) {
-            if (token.equals(this.tokens.get(lookahead))) {
-                return true;
-            }
-            lookahead++;
-        }
-        return false;
-    }
-
-    /**
-     * Warning! This method IS NOT side effect free, it changes lexer state.
-     */
-    public void moveTo(TokenInterface token) throws TokenNotFound {
-        if (this.tokens.size() <= this.position + 1) {
-            return;
-        }
-
-        while (this.position < this.tokens.size()) {
-            if (token.equals(this.tokens.get(this.position))) {
-                return;
-            }
-            this.next();
-        }
-
-        throw new TokenNotFound();
-    }
-
-    public TokenInterface getPrevious() {
-        int previousPosition = this.position - 1 >= 0 ? this.position - 1 : 0;
-        return this.tokens.get(previousPosition);
-    }
-
-    public boolean hasInvalidTokens() {
-        return this.invalidTokens;
-    }
-
-    public String lexedText() {
-        return this.lexedText;
-    }
-
-    @Override
-    public String toString() {
-        int tempPosition = this.position;
-        String tokensTexts = "";
-
-        while (tempPosition < this.tokens.size()) {
-            tokensTexts += this.tokens.get(tempPosition).getText();
-            tempPosition++;
-        }
-
-        return tokensTexts;
-    }
+    return tokensTexts;
+  }
 }
--- src/main/java/emailvalidator4j/parser/Parser.java
+++ src/main/java/emailvalidator4j/parser/Parser.java
@@ -1,133 +1,136 @@
 package emailvalidator4j.parser;
 
-import emailvalidator4j.lexer.EmailLexer;
-import emailvalidator4j.lexer.TokenInterface;
-import emailvalidator4j.lexer.Tokens;
-import emailvalidator4j.parser.exception.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import emailvalidator4j.lexer.EmailLexer;
+import emailvalidator4j.lexer.TokenInterface;
+import emailvalidator4j.lexer.Tokens;
+import emailvalidator4j.parser.exception.ATEXTAfterCFWS;
+import emailvalidator4j.parser.exception.ATEXTAfterComment;
+import emailvalidator4j.parser.exception.CRLFAtEnd;
+import emailvalidator4j.parser.exception.CRWithoutLF;
+import emailvalidator4j.parser.exception.ConsecutiveCRLF;
+import emailvalidator4j.parser.exception.ConsecutiveDots;
+import emailvalidator4j.parser.exception.ExpectedCTEXT;
+import emailvalidator4j.parser.exception.InvalidEmail;
+import emailvalidator4j.parser.exception.UnclosedComment;
+import emailvalidator4j.parser.exception.UnclosedDQUOTE;
+
 abstract class Parser {
-    protected List<Warnings> warnings = new ArrayList<Warnings>();
-    protected EmailLexer lexer;
+  protected List<Warnings> warnings = new ArrayList<>();
+  protected EmailLexer     lexer;
 
-    public Parser(EmailLexer lexer) {
-        this.lexer = lexer;
+  public Parser(EmailLexer lexer) {
+    this.lexer = lexer;
+  }
+
+  public abstract void parse(String part) throws InvalidEmail;
+
+  public List<Warnings> getWarnings() {
+    return this.warnings;
+  }
+
+  protected void validateQuotedPair() {
+    //only applies for emails like "\"@domain.com or "test\"@domain.com
+    this.warnings.add(Warnings.DEPRECATED_QP);
+  }
+
+  protected boolean escaped() {
+    return this.lexer.getPrevious().equals(Tokens.BACKSLASH) && !this.lexer.getCurrent().equals(Tokens.get("GENERIC"));
+  }
+
+  protected void parseComment() throws InvalidEmail {
+    this.checkUnclosedComment();
+    this.warnings.add(Warnings.COMMENT);
+
+    while (!this.lexer.getCurrent().equals(Tokens.CLOSEPARENTHESIS)) {
+      this.lexer.next();
     }
 
-    public abstract void parse(String part) throws InvalidEmail;
-
-    public List<Warnings> getWarnings() {
-        return this.warnings;
+    if (this.lexer.isNextToken(Tokens.get("GENERIC"))) {
+      throw new ATEXTAfterComment("");
     }
 
-    protected void validateQuotedPair() {
-        //only applies for emails like "\"@domain.com or "test\"@domain.com
-        this.warnings.add(Warnings.DEPRECATED_QP);
+    if (this.lexer.isNextToken(Tokens.AT)) {
+      this.warnings.add(Warnings.DEPRECATED_CFWS_NEAR_AT);
+    }
+  }
+
+  protected void checkUnclosedComment() throws InvalidEmail {
+    if (!this.lexer.find(Tokens.CLOSEPARENTHESIS)) {
+      throw new UnclosedComment("Unclosed Comment");
+    }
+  }
+
+  protected void checkConsecutiveDots() throws InvalidEmail {
+    if (this.lexer.getCurrent().equals(Tokens.DOT) && this.lexer.isNextToken(Tokens.DOT)) {
+      throw new ConsecutiveDots("Consecutive dots");
+    }
+  }
+
+  protected boolean checkDoubleQuote(boolean hasClosingQuote) throws InvalidEmail {
+    if (!this.lexer.getCurrent().equals(Tokens.DQUOTE)) {
+      return hasClosingQuote;
     }
 
-    protected boolean escaped() {
-        return this.lexer.getPrevious().equals(Tokens.BACKSLASH) && !this.lexer.getCurrent().equals(Tokens.get("GENERIC"));
+    if (hasClosingQuote) {
+      return hasClosingQuote;
     }
 
-    protected void parseComment() throws InvalidEmail {
-        this.checkUnclosedComment();
-        this.warnings.add(Warnings.COMMENT);
-
-        while (!this.lexer.getCurrent().equals(Tokens.CLOSEPARENTHESIS)) {
-            this.lexer.next();
-        }
-
-        if (this.lexer.isNextToken(Tokens.get("GENERIC"))) {
-            throw new ATEXTAfterComment("");
-        }
-
-        if (this.lexer.isNextToken(Tokens.AT)) {
-            this.warnings.add(Warnings.DEPRECATED_CFWS_NEAR_AT);
-        }
+    if (this.lexer.isNextToken(Tokens.get("GENERIC")) && this.lexer.getPrevious().equals(Tokens.get("GENERIC"))) {
+      //TODO review this condition.
     }
 
-    protected void checkUnclosedComment() throws InvalidEmail {
-        if (!this.lexer.find(Tokens.CLOSEPARENTHESIS)) {
-            throw new UnclosedComment("Unclosed Comment");
-        }
+    this.warnings.add(Warnings.RFC5321_QUOTEDSTRING);
+    hasClosingQuote = this.lexer.find(Tokens.DQUOTE);
+    if (!hasClosingQuote) {
+      throw new UnclosedDQUOTE("Unclosed DQUOTE");
+    }
+    return hasClosingQuote;
+  }
+
+  protected boolean isFWS() {
+    List<TokenInterface> FWSTokens = new ArrayList<TokenInterface>(
+        Arrays.asList(Tokens.HTAB, Tokens.SP, Tokens.CR, Tokens.LF, Tokens.CRLF));
+
+    return !this.escaped() && FWSTokens.contains(this.lexer.getCurrent());
+  }
+
+  protected void parseFWS() throws InvalidEmail {
+    this.checkCRLFInFWS();
+    if (this.lexer.getCurrent().equals(Tokens.CR)) {
+      throw new CRWithoutLF("Found CR but no LF");
     }
 
-    protected void checkConsecutiveDots() throws InvalidEmail {
-        if (this.lexer.getCurrent().equals(Tokens.DOT) && this.lexer.isNextToken(Tokens.DOT)) {
-            throw new ConsecutiveDots("Consecutive dots");
-        }
+    if (this.lexer.isNextToken(Tokens.get("GENERIC")) && !this.lexer.getPrevious().equals(Tokens.AT)) {
+      throw new ATEXTAfterCFWS("ATEXT found after CFWS");
     }
 
-
-    protected boolean checkDoubleQuote(boolean hasClosingQuote) throws InvalidEmail {
-        if (!this.lexer.getCurrent().equals(Tokens.DQUOTE)) {
-            return hasClosingQuote;
-        }
-
-        if (hasClosingQuote) {
-            return hasClosingQuote;
-        }
-
-        if (this.lexer.isNextToken(Tokens.get("GENERIC")) && this.lexer.getPrevious().equals(Tokens.get("GENERIC"))) {
-            //TODO review this condition.
-        }
-
-        this.warnings.add(Warnings.RFC5321_QUOTEDSTRING);
-        hasClosingQuote = this.lexer.find(Tokens.DQUOTE);
-        if (!hasClosingQuote) {
-            throw new UnclosedDQUOTE("Unclosed DQUOTE");
-        }
-        return hasClosingQuote;
+    if (this.lexer.getCurrent().equals(Tokens.LF) || this.lexer.getCurrent().equals(Tokens.NUL)) {
+      throw new ExpectedCTEXT("Expecting CTEXT");
     }
 
-    protected boolean isFWS() {
-        List<TokenInterface> FWSTokens = new ArrayList<TokenInterface>(Arrays.asList(
-                Tokens.HTAB,
-                Tokens.SP,
-                Tokens.CR,
-                Tokens.LF,
-                Tokens.CRLF
-        ));
+    if (this.lexer.isNextToken(Tokens.AT) || this.lexer.getPrevious().equals(Tokens.AT)) {
+      this.warnings.add(Warnings.DEPRECATED_CFWS_NEAR_AT);
+    } else {
+      this.warnings.add(Warnings.CFWS_FWS);
+    }
+  }
 
-        return !this.escaped() && FWSTokens.contains(this.lexer.getCurrent());
+  private void checkCRLFInFWS() throws InvalidEmail {
+    if (!this.lexer.getCurrent().equals(Tokens.CRLF)) {
+      return;
     }
 
-    protected void parseFWS() throws InvalidEmail {
-        this.checkCRLFInFWS();
-        if (this.lexer.getCurrent().equals(Tokens.CR)) {
-            throw new CRWithoutLF("Found CR but no LF");
-        }
-
-        if (this.lexer.isNextToken(Tokens.get("GENERIC")) && !this.lexer.getPrevious().equals(Tokens.AT)) {
-            throw new ATEXTAfterCFWS("ATEXT found after CFWS");
-        }
-
-        if (this.lexer.getCurrent().equals(Tokens.LF) || this.lexer.getCurrent().equals(Tokens.NUL)) {
-            throw new ExpectedCTEXT("Expecting CTEXT");
-        }
-
-        if (this.lexer.isNextToken(Tokens.AT) || this.lexer.getPrevious().equals(Tokens.AT)) {
-            this.warnings.add(Warnings.DEPRECATED_CFWS_NEAR_AT);
-        } else {
-            this.warnings.add(Warnings.CFWS_FWS);
-        }
+    if (this.lexer.getCurrent().equals(Tokens.CRLF)) {
+      if (this.lexer.isNextToken(Tokens.CRLF)) {
+        throw new ConsecutiveCRLF("Consecutive CRLF");
+      }
     }
-
-    private void checkCRLFInFWS() throws InvalidEmail {
-        if (!this.lexer.getCurrent().equals(Tokens.CRLF)) {
-            return;
-        }
-
-        if (this.lexer.getCurrent().equals(Tokens.CRLF)) {
-            if (this.lexer.isNextToken(Tokens.CRLF)) {
-                throw new ConsecutiveCRLF("Consecutive CRLF");
-            }
-        }
-        if (!this.lexer.isNextToken(new ArrayList<>(Arrays.asList(Tokens.SP, Tokens.HTAB)))) {
-            throw new CRLFAtEnd("CRLF at the end");
-        }
+    if (!this.lexer.isNextToken(new ArrayList<>(Arrays.asList(Tokens.SP, Tokens.HTAB)))) {
+      throw new CRLFAtEnd("CRLF at the end");
     }
+  }
 }
